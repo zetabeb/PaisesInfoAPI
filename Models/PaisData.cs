57aedceb7c5342bdadadf0c35bcb5156a272965b
zetabeb <zambenber@hotmail.com>
2022-06-16 00:08:00 -0500

    First commit

---
+++ Models/PaisData.cs
@@ -0,0 +1,16 @@
+namespace PaisesInfoAPI.Models
+{
+    public class PaisData
+    {
+        public int id{ get; set; }
+        public string nombre { get; set; } = string.Empty;
+        public string continente { get; set; } = string.Empty;
+        public string capital { get; set; } = string.Empty;
+        public string moneda { get; set; } = string.Empty;
+        public string indicativoTelefonia { get; set; } = string.Empty;
+        public string zonahoraria { get; set; } = string.Empty;
+        public string codigoPostal { get; set; } = string.Empty;
+        public string idioma { get; set; } = string.Empty:
+
+    }
+}
